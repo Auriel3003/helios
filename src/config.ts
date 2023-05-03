392880172914f981a50fbb47739fcbd294be0bc6
Auriel3003 <116882665+Auriel3003@users.noreply.github.com>
2023-05-03 20:22:36 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  author: "Helios Parker",
+  desc: "A minimal, responsive PROJECT Development Website for Engineers.",
+  title: "HELIOS",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
